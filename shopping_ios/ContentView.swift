b171a3e69edc1c4e1a6e4474ba50129730a2aeb2
sundayti <pankratovvlad1@gmail.com>
2025-06-20 22:49:30 +0300

    Initial Commit

---
+++ shopping_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shopping_ios
+//
+//  Created by Vladislav Pankratov on 20.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shopping_ios/shopping_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopping_iosApp.swift
+//  shopping_ios
+//
+//  Created by Vladislav Pankratov on 20.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct shopping_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
